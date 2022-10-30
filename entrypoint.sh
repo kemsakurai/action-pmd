068c0d13bee63b7b81245769bd6335218dacbd99
Ken Sakurai <monotalk.xyz@gmail.com>
2022-10-30 22:43:22 +0900

    Init commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,16 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-misspell -locale="${INPUT_LOCALE}" . \
-  | reviewdog -efm="%f:%l:%c: %m" \
-      -name="linter-name (misspell)" \
+printenv
+ls
+
+# fetch pmd of a requested version
+wget -q "https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.51.0/pmd-bin-6.51.0.zip"
+unzip pmd-bin-6.51.0.zip
+alias pmd="./pmd-bin-6.51.0/bin/run.sh pmd"
+exec pmd -d {INPUT_SRC_PATH} -R {INPUT_RULESETS_PATH} -f emacs \
+  | reviewdog -efm="%f:%l: %m" \
+      -name="PMD" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
       -filter-mode="${INPUT_FILTER_MODE}" \
       -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
