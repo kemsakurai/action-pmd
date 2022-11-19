42b856f2638ca7eb8bcf74c26024e26166cf443c
kemsakurai <sakurai.kem@mail.com>
2022-11-19 12:05:11 +0900

    close #3 Moved PMD setup process to Dockerfile.

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,11 +11,7 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 printenv
 ls
 
-# fetch pmd of a requested version
-wget -q "https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.51.0/pmd-bin-6.51.0.zip"
-unzip pmd-bin-6.51.0.zip
-
-exec /bin/sh ./pmd-bin-6.51.0/bin/run.sh pmd -d "${INPUT_SRC_PATH}" -R "${INPUT_RULESETS_PATH}" -f emacs \
+exec pmd -d "${INPUT_SRC_PATH}" -R "${INPUT_RULESETS_PATH}" -f emacs \
   | reviewdog -efm="%f:%l: %m" \
       -name="${INPUT_TOOL_NAME}" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
